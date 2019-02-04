fcf0e3223f8b78c1d8c08fd851cf6e22380d5fc0
Ixax Tavarez <i.tavarez@umiami.edu>
2019-02-04 16:45:09 +0000

    coin css added

--- src/js/component/Settings/CoinGrid.jsx
+++ src/js/component/Settings/CoinGrid.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import { AppContext } from "../../store/AppProvider.js";
 import PropTypes from "prop-types";
+import { SelectableTile } from "../../component/Shared/Tile.jsx";
 
 export const CoinGridStyled = styled.div`
 	display: grid;
 	grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
+	grid-gap: 15px;
 `;
 
 export default function CoinGrid() {
@@ -14,7 +16,10 @@ export default function CoinGrid() {
 			{({ coinList }) => (
 				<CoinGridStyled>
 					{Object.keys(coinList).map(coinKey => (
-						<div key> {coinKey} </div>
+						<SelectableTile key={coinKey}>
+							{" "}
+							{coinKey}{" "}
+						</SelectableTile>
 					))}
 				</CoinGridStyled>
 			)}
--- src/js/component/Settings/ConfirmButton.jsx
+++ src/js/component/Settings/ConfirmButton.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import styled, { css } from "styled-components";
 import PropTypes from "prop-types";
 import { AppContext } from "../../store/AppProvider.js";
+import { fontSize1, greenBoxShadow, color3 } from "../Shared/Styles.jsx";
 
 const ButtonStyled = styled.div`
-	margin: 20px;
-	color: green;
-	cursor: pointer;
+  margin: 20px;
+  color: ${color3}
+  ${fontSize1} 
+  padding: 5px;
+  cursor: pointer; 
+  &:hover {
+    ${greenBoxShadow} 
+  }
 `;
 
 export const CenterDiv = styled.div`
@@ -18,9 +24,11 @@ export default function ConfirmButton() {
 	return (
 		<AppContext.Consumer>
 			{({ confirmFavorites }) => (
-				<ButtonStyled onClick={confirmFavorites}>
-					Confirm Favorites
-				</ButtonStyled>
+				<CenterDiv>
+					<ButtonStyled onClick={confirmFavorites}>
+						Confirm Favorites
+					</ButtonStyled>
+				</CenterDiv>
 			)}
 		</AppContext.Consumer>
 	);
