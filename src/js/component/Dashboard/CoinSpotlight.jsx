9560aa779777e41bde5d779bdc90b2fb5a1b87d2
Ixax Tavarez <i.tavarez@umiami.edu>
2019-02-12 19:51:00 +0000

    chart select css added

--- src/js/component/Dashboard/CoinSpotlight.jsx
+++ src/js/component/Dashboard/CoinSpotlight.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Tile} from "../Shared/Tile";
-import {AppContext} from "../App/AppProvider";
-import CoinImage from '../Shared/CoinImage';
+import {Tile} from "../../component/Shared/Tile.jsx";
+import {AppContext} from "../store/AppProvider.jsx";
+import CoinImage from '../../component/Shared/CoinImage.jsx';
 
 const SpotlightName = styled.h2`
   text-align: center; 
